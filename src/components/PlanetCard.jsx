0a59a88744a49ec356c308ec1ab3c58a74561ca5
Jorge Dorio <dorio@pop-os.localdomain>
2022-01-09 22:50:08 -0400

    Renderize uma lista com os planetas do Sistema Solar

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card" className="planet">
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } className="planet-img" />
+        <h3 data-testid="planet-name">{planetName}</h3>
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
